722d4984ba4ee00b67d484a8681feef8d62f3f92
manisha1896 <manisha.4918@gmail.com>
2018-11-24 20:37:53 +0530

    functionality included

---
+++ battle.js
@@ -0,0 +1,7 @@
+window.onload=battle;
+function battle()
+{
+              var shot="G3";
+             document.getElementById(shot).style.backgroundImage="url('miss.png')";
+             document.getElementById("result").innerHTML="Good Shot";
+}
